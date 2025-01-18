17fea749535f48f722decbc3bf819d4104e1382d
sarahib37 <129744358+sarahib37@users.noreply.github.com>
2025-01-18 18:40:11 +0100

    Update index.mjs

--- Backend/api/index.mjs
+++ Backend/api/index.mjs
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://hclerald-flnf.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
